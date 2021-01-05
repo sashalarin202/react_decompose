e05f51f13ec1d9d4ca6702fa249067f3523be1dd
Sasha Larin <sashalarin200@gmail.com>
2021-01-05 20:16:48 +0200

    Updates

---
+++ precache-manifest.813fe5d30aedc6aa1f7aa8da13ef7b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3690452265c319b0f22089d51262bc63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c066e38dcf1adbb3465",
+    "url": "./static/css/main.f6f38deb.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c066e38dcf1adbb3465",
+    "url": "./static/js/main.0e0dae4b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
